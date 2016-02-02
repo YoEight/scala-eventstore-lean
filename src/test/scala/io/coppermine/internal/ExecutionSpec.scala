40dd13b40d6562f7a29d6497508c68fd9b498da1
YoEight <yo.eight@gmail.com>
2016-02-02 10:55:47 +0100

    Simplify Manager use in tests

--- src/test/scala/io/coppermine/internal/ExecutionSpec.scala
+++ src/test/scala/io/coppermine/internal/ExecutionSpec.scala
@@ -9,24 +9,28 @@ object ExecutionSpec extends Specification { def is = s2"""
     writing an event should work      $writeEvents
                                       """
 
+  val settings = Settings("localhost", 1113)
+
   def writeEvents = {
-    val settings  = Settings("localhost", 1113)
-    val conn      = Connection(settings)
-    val evt       = Event("event-type", BinaryData("event-data".getBytes, None))
-    val result    = new SyncVar[WriteResult]()
-    val cmd       = command.SendEvents("test-stream", List(evt), true, AnyVersion, result)
-    val mgr       = Manager(settings)
+    val conn   = Connection(settings)
+    val evt    = Event("event-type", BinaryData("event-data".getBytes, None))
+    val result = new SyncVar[WriteResult]()
+    val cmd    = command.SendEvents("test-stream", List(evt), true, AnyVersion, result)
+    val mgr    = Manager(settings)
 
-    val SendPackage(pkg, nextMgr) = mgr(cmd)
-    conn.send(pkg)
-
-    val resp = waitResponse(conn)
-    nextMgr(resp) match {
-      case Some(Await(_)) => /* Nothing to do here */
-      case err            => sys.error(s"Wrong response $err")
+    val action = for {
+      pkg <- ManagerM.addCommand(cmd)
+      _    = conn.send(pkg)
+      resp = waitResponse(conn)
+      out <- ManagerM.handlePackage(resp)
+    } yield out match {
+      case MgrNoop => result.get
+      case wrong   => sys.error(s"Wrong response $wrong")
     }
 
-    result.get must not beNull
+    val write = action.execute(settings)
+
+    write must not beNull
   }
 
   @annotation.tailrec
---
+++ src/test/scala/io/coppermine/internal/ManagerM.scala
@@ -0,0 +1,45 @@
+package io.coppermine
+package internal
+
+sealed trait ManagerOut
+
+case class MgrSend(pkg: Package) extends ManagerOut
+case class MgrRetry(cmd: Command) extends ManagerOut
+case object MgrNoop extends ManagerOut
+
+case class ManagerM[A](k: Manager => (Manager, A)) {
+  def map[B](f: (A) => B): ManagerM[B] = ManagerM { m =>
+    k(m) match {
+      case (newM, a) => (newM, f(a))
+    }
+  }
+
+  def flatMap[B](f: (A) => ManagerM[B]): ManagerM[B] = ManagerM { m =>
+    k(m) match {
+      case (newM, a) => f(a).k(newM)
+    }
+  }
+
+  def execute(settings: Settings): A = k(Manager(settings))._2
+}
+
+object ManagerM {
+  def point[A](v: => A): ManagerM[A] = ManagerM(m => (m, v))
+
+  def addCommand(cmd: Command): ManagerM[Package] = ManagerM { m =>
+    m(cmd) match {
+      case SendPackage(pkg, nxtM) => (nxtM, pkg)
+    }
+  }
+
+  def handlePackage(pkg: Package): ManagerM[ManagerOut] = ManagerM { m =>
+    m(pkg) match {
+      case None      => (m, MgrNoop)
+      case Some(res) => res match {
+        case SendPackage(pkg, newM)   => (newM, MgrSend(pkg))
+        case PushOperation(cmd, newM) => (newM, MgrRetry(cmd))
+        case Await(newM)              => (newM, MgrNoop)
+      }
+    }
+  }
+}
